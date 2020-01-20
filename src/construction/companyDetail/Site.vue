7d5edcc44fabbbd09bbe1e4b4f5d1c2a5a956665
linglanfei <435837284@qq.com>
2020-01-20 10:01:43 +0800

    重复渲染

--- src/construction/companyDetail/Site.vue
+++ src/construction/companyDetail/Site.vue
@@ -1,6 +1,6 @@
 <template>
 <div style="height: auto;width: 100%;" class="all">
-  <div class="site" v-for="site of ss" v-if="pa.indexOf('constructionSite') > 0||pa.indexOf('siteList') > 0" @click="$router.push({path:'/siteDetail',query:{id:site.id}})">
+  <div class="site" v-for="site of sites" @click="$router.push({path:'/siteDetail',query:{id:site.id}})">
     <img :src="site.thumb_img"/>
     <div>
       <p class="heavy" >{{site.quarter_name}} | {{site.shape_name}}</p>
@@ -32,33 +32,42 @@ export default {
   methods: {
     handleFun () {
       var _this = this
-      window.onscroll = function () {
-        _this.scroll = (window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop) >= 400
-        // 变量scrollTop是滚动条滚动时，距离顶部的距离
-        var scrollTop = document.documentElement.scrollTop || document.body.scrollTop
-        // 变量windowHeight是可视区的高度
-        var windowHeight = document.documentElement.clientHeight || document.body.clientHeight
-        // 变量scrollHeight是滚动条的总高度
-        var scrollHeight = document.documentElement.scrollHeight || document.body.scrollHeight
-        // 滚动条到底部的条件
-        if (scrollTop + windowHeight === scrollHeight) {
-          if (_this.pa.indexOf('constructionSite') > 0) {
-            _this.param.lastIndex = _this.ss.length
-            _this.$ajax.get('/api/construction/site', {params: _this.param}).then((res) => {
+      _this.scroll = (window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop) >= 400
+      // 变量scrollTop是滚动条滚动时，距离顶部的距离
+      var scrollTop = document.documentElement.scrollTop || document.body.scrollTop
+      // 变量windowHeight是可视区的高度
+      var windowHeight = document.documentElement.clientHeight || document.body.clientHeight
+      // 变量scrollHeight是滚动条的总高度
+      var scrollHeight = document.documentElement.scrollHeight || document.body.scrollHeight
+      // 滚动条到底部的条件
+      if (scrollTop + windowHeight === scrollHeight) {
+        if (_this.pa.indexOf('constructionSite') > 0) {
+          _this.param.lastIndex = _this.sites === undefined ? 0 : _this.sites.length
+          if (this.param.lastIndex > 0) {
+            this.$ajax.get('/api/construction/site', {params: _this.param}).then((res) => {
               _this.ss = _this.ss.concat(res.data.data)
             })
-          } else {
-            _this.$emit('up')
           }
+        } else {
+          _this.$emit('up')
         }
       }
     }
   },
-  created () {
+  mounted () {
+    let _this = this
+    window.scroll(0, 0)
     window.addEventListener('scroll', this.handleFun)
-    this.$ajax.get('/api/construction/site', {params: this.param}).then((res) => {
-      this.ss = res.data.data
-    })
+    if (_this.sites === undefined) {
+      _this.$ajax.get('/api/construction/site?company_id= ' + this.$route.query.id + '& itemsPerLoad: 10&lastIndex: 0').then((res) => {
+        _this.ss = res.data.data
+      })
+    }
+  },
+  computed: {
+    sites: function () {
+      return this.ss
+    }
   }
 }
 </script>
